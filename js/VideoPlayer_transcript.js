1114fb14b06e624bfaa0d6f07440778e582b3cb5
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-10-04 11:46:04 -0400

    FLUID-4792: Ensure transcript is not re-rendered unless it has to be

--- js/VideoPlayer_transcript.js
+++ js/VideoPlayer_transcript.js
@@ -364,6 +364,12 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
         });
 
         that.applier.modelChanged.addListener("currentTracks.transcripts", function (model, oldModel) {
+            if (model.currentTracks.transcripts[0] === oldModel.currentTracks.transcripts[0]) {
+                // actual choice of track hasn't changed; just show it and be done
+                fluid.videoPlayer.transcript.showTranscriptArea(that);
+                return;
+            }
+
             fluid.videoPlayer.transcript.prepareTranscript(that);
             
             // Select the new transcript in the drop down list box
