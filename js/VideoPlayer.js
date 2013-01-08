a363ab371a115a41c44299ae0e62fb5ebf6a823a
Anastasia Cheetham <ac@anastasiacheetham.ca>
2013-01-08 12:38:31 -0500

    FLUID-4822: Updating copyright year

--- js/VideoPlayer.js
+++ js/VideoPlayer.js
@@ -1,7 +1,7 @@
 /*
 Copyright 2009 University of Toronto
 Copyright 2011 Charly Molter
-Copyright 2011-2012 OCAD University
+Copyright 2011-2013 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
