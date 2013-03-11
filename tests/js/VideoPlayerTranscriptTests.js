81f19fead92e888c38ab4ff5527f63149f2b142e
Alexey Novak <abovebluesky@gmail.com>
2013-03-11 16:53:44 -0400

    VP-211: Fixing up transcripts to use languages and captions pulled from Amara.

--- tests/js/VideoPlayerTranscriptTests.js
+++ tests/js/VideoPlayerTranscriptTests.js
@@ -71,7 +71,7 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
             that.applier.requestChange("displayTranscripts", true);
             
             jqUnit.isVisible("The transcript area is shown", $(".flc-videoPlayer-transcriptArea"));
-            jqUnit.assertEquals("The language combo box has expected number of options", that.options.transcripts.length, $(".flc-videoPlayer-transcripts-language-dropdown option").length);
+            jqUnit.assertEquals("The language combo box has expected number of options", that.options.model.transcripts.length, $(".flc-videoPlayer-transcripts-language-dropdown option").length);
             jqUnit.assertTrue("The default selected language is the first option", $(".flc-videoPlayer-transcripts-language-dropdown option")[0].selected);
         };
         
@@ -129,6 +129,7 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
         };
 
         var localTranscriptOpts = {
+            model: {
                 transcripts: [
                     {
                         src: "TestTranscripts.en.json",
@@ -143,33 +144,36 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
                         label: "French"
                     }
                 ]
-            };
+            }
+        };
 
         // Load transcript files from universal subtitles
         var universalSubsOpts = {
-            transcripts: [
-                {
-                    src: "http://www.youtube.com/watch?v=_VxQEPw1x9E&language=en",
-                    type: "text/amarajson",
-                    srclang: "en",
-                    label: "English"
-                },
-                {
-                    src: "http://www.youtube.com/watch?v=_VxQEPw1x9E&language=fr",
-                    type: "text/amarajson",
-                    srclang: "fr",
-                    label: "French"
-                }
-            ]
+            model: {
+                transcripts: [
+                    {
+                        src: "http://www.youtube.com/watch?v=_VxQEPw1x9E&language=en",
+                        type: "text/amarajson",
+                        srclang: "en",
+                        label: "English"
+                    },
+                    {
+                        src: "http://www.youtube.com/watch?v=_VxQEPw1x9E&language=fr",
+                        type: "text/amarajson",
+                        srclang: "fr",
+                        label: "French"
+                    }
+                ]
+            }
         };
         
         testProcess(localTranscriptOpts, "Local transcript files");
         
-        // Wait two seconds for the previous test process to complete
+        // Wait a second for the previous test process to complete
         // TODO: It should be changed to use promises instead
         setTimeout(function () {
             testProcess(universalSubsOpts, "Universal Subtitle transcript files");
-        }, 2000);
+        }, 1000);
 
         jqUnit.asyncTest("Drop-down aria-controls text area", function () {
             var testOpts = {
